edaf545988b2168549844edb51bdb7a8bf29fb0a
Darshan Datta <darshandatta.10@gmail.com>
2020-04-27 15:49:10 +0530

    created and connected to redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,34 @@ import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <section className="container">
-          <Switch>
-            <Route exact path="/register">
-              <Register />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <section className="container">
+            <Switch>
+              <Route exact path="/register">
+                <Register />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
