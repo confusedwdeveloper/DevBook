c9c17014f790f5009f596952cd7238ea071084c5
Darshan Datta <darshandatta.10@gmail.com>
2020-05-12 01:06:31 +0530

    created profile actions and reducers

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,31 @@
+import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+
+const initialState = {
+  profile: null,
+  profiles: [], //for list of developers
+  repos: [],
+  loading: true,
+  error: {},
+};
+
+export default function profile(state = initialState, { type, payload }) {
+  switch (type) {
+    case GET_PROFILE: {
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    }
+    case PROFILE_ERROR: {
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
